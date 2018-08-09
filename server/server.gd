13e131b1c1b1f7ddfc36056d499c886ab37c8f2b
kiesenverseist <creativeibi77@gmail.com>
2018-08-09 17:47:11 +1000

    Added names and Data sharing
    Wrote a system for sharing player data, and with that added player names
    Also Made the settings page have purpose.

--- server/server.gd
+++ server/server.gd
@@ -1,15 +1,13 @@
 extends Node
 
-#server parameters
-const PORT = 8080
-const MAX_PLAYERS = 32
-
 var player_pk = preload("res://client/player/player.tscn")
+sync var players = {}
+
 
 func _ready():
 	#start server
 	var server = NetworkedMultiplayerENet.new()
-	server.create_server(PORT, MAX_PLAYERS)
+	server.create_server($"/root/globals".settings.port, $"/root/globals".settings.player_limit)
 	get_tree().set_network_peer(server)
 	
 	#connect functions
@@ -17,16 +15,26 @@ func _ready():
 	get_tree().connect("network_peer_disconnected", self, "_client_disconnected")
 
 func _client_connected(id):
-	print("client %s connected".format(id))
+	print("client %s connected" % str(id))
 	var player = player_pk.instance()
 	player.name = String(id)
 	player.set_network_master(id)
 	$world/players.add_child(player)
+	rpc_id(id, "get_player_inf")
 
 func _client_disconnected(id):
-	print("client %s disconnected".format(id))
+	print("client %s disconnected" % str(id))
 	
 	get_node("world/players/" + str(id)).queue_free()
-
-remote func make_player(id):
 	
+	players.erase(id)
+
+remote func register_player(id, inf):
+	players[id] = inf
+	rset("players", players)
+	rpc("update_players")
+	update_players()
+
+func update_players():
+	for p in $"world/players".get_children():
+		p.get_node("Name").text = players[int(p.name)]["player_name"]
