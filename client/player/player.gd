8d66958adca8244008e22e6b0b6daf7140dff206
Unknown <creativeibi77@gmail.com>
2018-08-12 20:27:46 +1000

    Character Physics and GUI
    Put more emphasis on the character by placing movement processing in it, which influences its controller.
    Added player physics, so they can fall jump and move.
    Added a simple GUI, an issue is that you interact behind the GUI.

--- client/player/player.gd
+++ client/player/player.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-var speed = 100
-var movement = Vector2(0,0)
-
 func _ready():
 	$Name.text = $"/root/globals".settings.player_name
 
@@ -11,7 +8,7 @@ func _input(event):
 	
 
 func _process(delta):
-	movement = Vector2(0,0)
+	var movement = Vector2(0,0)
 	
 	if Input.is_action_pressed("move_right"):
 		movement.x += 1
@@ -22,11 +19,14 @@ func _process(delta):
 	if Input.is_action_pressed("move_down"):
 		movement.y += 1
 	
-	position += movement * speed * delta
+	var sprint = Input.is_key_pressed(KEY_SHIFT)
+	
+	$character.rpc_unreliable("move", movement, sprint)
 
 func _draw():
 	if is_network_master():
-		draw_rect(Rect2(-Vector2(34, 34), Vector2(68, 68)), Color(1, 0, 0), false)
+		draw_rect(Rect2(-Vector2(18, 34), Vector2(34, 68)), Color(1, 0, 0, 0.5), false)
 
 func _network_tick():
-	rpc("update_movement", movement, position)
+	pass
+#	rpc("update_movement", movement, position)
