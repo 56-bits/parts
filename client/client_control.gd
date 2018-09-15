a6589dcbb62c5ec8f73880d9c44a6dec1aed1d75
kiesenversist <creativeibi77@gmail.com>
2018-09-15 20:35:10 +1000

    Client side island editing

--- client/client_control.gd
+++ client/client_control.gd
@@ -6,20 +6,32 @@ var alt_tile_type = -1
 var click = false
 var alt_click = false
 
+var target = null
+onready var m_col = $"../mouse_col"
+
 func _ready():
-	$"../gui/ItemList".connect("item_selected", self, "_on_ItemList_item_selected")
+	$"../gui/VSplitContainer2/ItemList".connect("item_selected", self, "_on_ItemList_item_selected")
 
 func _process(delta):
 	
-	if click:
-		$"../world".rpc("edit_terrain", get_local_mouse_position(), tile_type)
-	if alt_click:
-		$"../world".rpc("edit_terrain", get_local_mouse_position(), alt_tile_type)
+	if Input.is_key_pressed(KEY_CONTROL) and Input.is_action_pressed("primary_click"):
+		m_col.monitoring = true
+	else:
+		m_col.monitoring = false
+		
+		if target != null:
+			if click:
+				target.rpc("edit_terrain", target.get_local_mouse_position(), tile_type)
+			if alt_click:
+				target.rpc("edit_terrain", target.get_local_mouse_position(), alt_tile_type)
+	
+	$"../mouse_col".position = get_local_mouse_position()
 	
 
 func _unhandled_input(event):
 	if event.is_action_pressed("primary_click"):
 		click = true
+		
 	if event.is_action_released("primary_click"):
 		click = false
 	
@@ -33,3 +45,7 @@ func _on_ItemList_item_selected(index):
 
 func _on_ItemList_item_rmb_selected(index, at_position):
 	alt_tile_type = index - 1
+
+func _on_mouse_col_body_entered(body):
+	target = body
+	$"../gui".target = body.name
